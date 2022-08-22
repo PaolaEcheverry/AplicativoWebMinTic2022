0978e0a9b35d73d91c99d0774faf72be6386d69f
zuluaga94 <42913649+zuluaga94@users.noreply.github.com>
2022-08-21 19:50:20 -0500

    Proyecto Web

--- PruebaVersiones1/src/main/java/Tareas.java
+++ PruebaVersiones1/src/main/java/Tareas.java
@@ -3,5 +3,11 @@ public class Tareas {
 public static void main (String args []){
 
     System.out.println("Mi nombre es Paola Echeverry");
+    System.out.println("Mi nombre es Sebastian Zuluaga Zapata");
+
 }
 }
+
+
+
+
